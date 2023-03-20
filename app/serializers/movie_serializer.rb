6d607a94f64ecdf01893d645056fdca109237321
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-20 09:02:46 +0300

    serializer associations

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
+  attributes :title, :year, :length, :description, :poster_url, :category, :discount
+
+  belongs_to :director
   
 end
